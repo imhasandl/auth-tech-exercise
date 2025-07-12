c017ddd3ed386c5d465068d9ef8046976d9a2407
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-07-13 01:46:19 +0300

    database connection and models

---
+++ database/models.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID         uuid.UUID `json:"id"`
+	Email      string    `json:"email"`
+	Password   string    `json:"password"`
+	Created_At time.Time `json:"created_at"`
+}
+
+type RefreshToken struct {
+	ID        uuid.UUID `json:"id"`
+	UserID    uuid.UUID `json:"user_id"`
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+	CreatedAt time.Time `json:"created_at"`
+}
--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-func main(){
-	fmt.Println("Hello")
+	"github.com/imhasandl/auth-tech-exercise/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Can not load .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Set port in .env file")
+	}
+
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("Set database url in .env file")
+	}
+
+	db, err := database.InitDatabase(db_url)
+	if err != nil {
+		log.Fatalf("failed connect to database: %v", err)
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	fmt.Println("Staring server on port 8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
