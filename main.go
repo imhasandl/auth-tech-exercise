18bcf095cb79797d367f7ea12bfd741f653e1bab
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-07-14 03:44:47 +0300

    handle register user

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/imhasandl/auth-tech-exercise/cmd/handlers"
 	"github.com/imhasandl/auth-tech-exercise/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -21,6 +22,11 @@ func main() {
 		log.Fatal("Set port in .env file")
 	}
 
+	tokenSecret := os.Getenv("TOKEN_SECRET")
+	if tokenSecret == "" {
+		log.Fatal("Set token secret in .env file")
+	}
+
 	db_url := os.Getenv("DB_URL")
 	if db_url == "" {
 		log.Fatal("Set database url in .env file")
@@ -32,7 +38,12 @@ func main() {
 	}
 	defer db.Close()
 
+	apiConfig := handlers.NewConfig(db, tokenSecret)
+
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /register", apiConfig.Register)
+	mux.HandleFunc("POST /login", apiConfig.Login)
 
 	server := &http.Server{
 		Addr:    ":" + port,
